40b3b5c246cb2ac66d5a1db0a571d63e0d9389f9
santiago restrepo <srest031@fiu.edu>
2023-01-18 12:48:12 -0500

    updated server.js

--- helpers/server.js
+++ helpers/server.js
@@ -4,7 +4,7 @@ const http = require('http')
 const cors = require('cors')
 
 // SERVER CONFIG
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 const app = express();
 const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${PORT}\n`))
 app.use(express.static(path.join(__dirname, 'public')))
